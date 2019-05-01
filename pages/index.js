e399675e0bfb2dc575255f8635578ed3d7c7d58e
jfedgar <jfedgar@gmail.com>
2019-05-01 18:04:47 -0400

    Add annotated layout page and index

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { EmptyState, Layout, Page, TextStyle } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+const Index = () => (
+  <Page
+    primaryAction={{
+      content: 'Select products',
+    }}
+  >
+    <Layout>
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+    </Layout>
+  </Page>
+);
+
+export default Index;
